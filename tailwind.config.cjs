62043d6f17df0b7dcf1f42b2216c35ffbe9753b1
Akash Raju M <akash@classcardapp.com>
2022-01-09 11:59:34 +0530

    add custom font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,25 @@
 module.exports = {
 	content: ['./src/**/*.html', './src/**/*.svelte'],
 	theme: {
+		fontFamily: {
+			sans: [
+				'Rubik',
+				'ui-sans-serif',
+				'system-ui',
+				'-apple-system',
+				'BlinkMacSystemFont',
+				'Segoe UI',
+				'Roboto',
+				'Helvetica Neue',
+				'Arial',
+				'Noto Sans',
+				'sans-serif',
+				'Apple Color Emoji',
+				'Segoe UI Emoji',
+				'Segoe UI Symbol',
+				'Noto Color Emoji'
+			]
+		},
 		extend: {}
 	},
 	plugins: [require('@tailwindcss/forms')]
