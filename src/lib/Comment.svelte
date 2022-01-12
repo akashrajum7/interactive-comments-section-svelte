04156a54704ebac6bfd6672b26079a2df36e5781
Akash Raju M <akash@classcardapp.com>
2022-01-12 14:33:02 +0530

    add reply text box

--- src/lib/Comment.svelte
+++ src/lib/Comment.svelte
@@ -2,31 +2,35 @@
 	import ReplyButton from './ReplyButton.svelte';
 	import UpvoteCounterHorizontal from './UpvoteCounterHorizontal.svelte';
 	import UpvoteCounterVertical from './UpvoteCounterVertical.svelte';
+	import ReplyTextField from './ReplyTextField.svelte';
 </script>
 
-<section class="bg-white p-4 rounded-lg overflow-hidden md:flex md:p-6 md:space-x-6">
-	<div class="hidden md:block">
-		<UpvoteCounterVertical />
-	</div>
-	<div class="space-y-4 ">
-		<div class="flex justify-between items-center">
-			<div class="flex items-center space-x-4">
-				<img src="/image-amyrobson.png" alt="dp" class="w-8 h-8" />
-				<h4 class="font-medium text-dark-blue">amyrobson</h4>
-				<span class="text-grayish-blue">1 month ago</span>
+<div class="space-y-2">
+	<section class="bg-white p-4 rounded-lg overflow-hidden md:flex md:p-6 md:space-x-6">
+		<div class="hidden md:block">
+			<UpvoteCounterVertical />
+		</div>
+		<div class="space-y-4 ">
+			<div class="flex justify-between items-center">
+				<div class="flex items-center space-x-4">
+					<img src="/image-amyrobson.png" alt="dp" class="w-8 h-8" />
+					<h4 class="font-medium text-dark-blue">amyrobson</h4>
+					<span class="text-grayish-blue">1 month ago</span>
+				</div>
+				<div class="hidden md:block">
+					<ReplyButton />
+				</div>
 			</div>
-			<div class="hidden md:block">
+			<p class="text-grayish-blue">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, harum blanditiis? Id
+				dicta exercitationem alias consequatur reprehenderit a, quas, ex provident blanditiis at
+				temporibus quidem eius, voluptatem nesciunt? Libero, accusamus.
+			</p>
+			<div class="flex justify-between md:hidden">
+				<UpvoteCounterHorizontal />
 				<ReplyButton />
 			</div>
 		</div>
-		<p class="text-grayish-blue">
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, harum blanditiis? Id dicta
-			exercitationem alias consequatur reprehenderit a, quas, ex provident blanditiis at temporibus
-			quidem eius, voluptatem nesciunt? Libero, accusamus.
-		</p>
-		<div class="flex justify-between md:hidden">
-			<UpvoteCounterHorizontal />
-			<ReplyButton />
-		</div>
-	</div>
-</section>
+	</section>
+	<ReplyTextField />
+</div>
---
+++ src/lib/ReplyTextField.svelte
@@ -0,0 +1,20 @@
+<section
+	class="bg-white p-4 rounded-lg overflow-hidden flex flex-col md:flex-row md:p-6 md:space-x-4"
+>
+	<img src="/image-amyrobson.png" alt="dp" class="w-9 h-9 hidden md:block" />
+	<textarea
+		class="flex-grow rounded-lg border border-moderate-blue px-6 py-3 text-base text-dark-blue focus:outline-none"
+		rows="3"
+		placeholder="Add a comment..."
+	/>
+	<button
+		class="text-white bg-moderate-blue rounded-lg px-6 py-2.5 hover:opacity-50 h-fit hidden md:block"
+		>REPLY</button
+	>
+	<div class="flex justify-between items-center mt-4 md:hidden">
+		<img src="/image-amyrobson.png" alt="dp" class="w-8 h-8" />
+		<button class="text-white bg-moderate-blue rounded-lg px-6 py-2.5 hover:opacity-50 h-fit"
+			>REPLY</button
+		>
+	</div>
+</section>
